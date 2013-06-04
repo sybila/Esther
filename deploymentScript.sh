733efc3b3a1ea530d1a7ee775d87a56d1a03db42
George Kolcak <Jooji.Kolcak@Gmail.com>
2013-06-04 16:56:10 +0200

    Deployment script repaired.

--- deploymentScript.sh
+++ deploymentScript.sh
@@ -6,7 +6,7 @@ read mysql_pass
 sed -i -e "s#jdbc\.username=.*#jdbc\.username=$mysql_user#" ./Sources/Esther/src/main/resources/Properties/Database.properties
 sed -i -e "s#jdbc\.password=.*#jdbc\.password=$mysql_pass#" ./Sources/Esther/src/main/resources/Properties/Database.properties
 
-mysql -u$mysql_user -p$mysql_pass -e "CREATE DATABADE IF NOT EXISTS EsherDB;"
+mysql -u$mysql_user -p$mysql_pass -e "CREATE DATABASE IF NOT EXISTS EstherDB;"
 mysql -u$mysql_user -p$mysql_pass EstherDB < createTables.sql
 
 echo "Please specify the path to Parsybone binary:"
@@ -22,7 +22,8 @@ chmod 777 $data_loc
 
 sed -i -e "s#data_location=.*#data_location=$data_loc#" ./Sources/Esther/src/main/resources/Properties/Esther.properties
 
-set repo_loc=data_loc + "mvn_repo/"
+repo_loc=$data_loc
+repo_loc+="mvn_repo/"
 
 mkdir $repo_loc
 
@@ -50,42 +51,42 @@ sed -i -e "s#captcha_private_key=.*#captcha_private_key=$captcha_private#" ./Sou
 
 mvn package -f Sources/EstherHeart/pom.xml
 mvn deploy:deploy-file -f Sources/EstherHeart/pom.xml \
--Durl=file:///repo_loc \
+-Durl=file://$repo_loc \
 -DgroupId=mu.fi.sybila -DartifactId=esther-heart \
 -Dversion=1.0 -Dfile=target/esther-heart-1.0.jar \
 -Dpackaging=jar -DgeneratePom=true
 
 mvn package -f Sources/SQLiteManager/pom.xml
 mvn deploy:deploy-file -f Sources/SQLiteManager/pom.xml \
--Durl=file:///repo_loc \
+-Durl=file://$repo_loc \
 -DgroupId=mu.fi.sybila -DartifactId=esther-sqlite \
 -Dversion=1.0 -Dfile=target/esther-sqlite-1.0.jar \
 -Dpackaging=jar -DgeneratePom=true
 
 mvn package -f Sources/ModelEditorWidget/pom.xml
 mvn deploy:deploy-file -f Sources/ModelEditorWidget/pom.xml \
--Durl=file:///repo_loc \
+-Durl=file://$repo_loc \
 -DgroupId=mu.fi.sybila -DartifactId=esther-model_editor_widget \
 -Dversion=1.0 -Dfile=target/esther-model_editor_widget-1.0.jar \
 -Dpackaging=jar -DgeneratePom=true
 
 mvn package -f Sources/ParsyboneWidget/pom.xml
 mvn deploy:deploy-file -f Sources/ParsyboneWidget/pom.xml \
--Durl=file:///repo_loc \
+-Durl=file://$repo_loc \
 -DgroupId=mu.fi.sybila -DartifactId=esther-parsybone_widget \
 -Dversion=1.0 -Dfile=target/esther-parsybone_widget-1.0.jar \
 -Dpackaging=jar -DgeneratePom=true
 
 mvn package -f Sources/ParameterViewWidget/pom.xml
 mvn deploy:deploy-file -f Sources/ParameterViewWidget/pom.xml \
--Durl=file:///repo_loc \
+-Durl=file://$repo_loc \
 -DgroupId=mu.fi.sybila -DartifactId=esther-parameter_view_widget \
 -Dversion=1.0 -Dfile=target/esther-parameter_view_widget-1.0.jar \
 -Dpackaging=jar -DgeneratePom=true
 
 mvn package -f Sources/BehaviourMapWidget/pom.xml
 mvn deploy:deploy-file -f Sources/BehaviourMapWidget/pom.xml \
--Durl=file:///repo_loc \
+-Durl=file://$repo_loc \
 -DgroupId=mu.fi.sybila -DartifactId=esther-behaviour_map_widget \
 -Dversion=1.0 -Dfile=target/esther-behaviour_map_widget-1.0.jar \
 -Dpackaging=jar -DgeneratePom=true
